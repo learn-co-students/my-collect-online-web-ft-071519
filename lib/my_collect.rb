ea1cdcf1a37bab397893179a82007660d4289043
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-16 18:09:33 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
 
+def my_collect(array)
+  i = 0
+  newArray = []
+  while i<array.length
+    newArray.push(yield array[i])
+    i+=1
+  end
+  return newArray
+end
 
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
